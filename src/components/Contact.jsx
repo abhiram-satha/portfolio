24f2bf3156494ed4d8c5af5a86db32ebaa4ff773
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-07-03 22:28:32 +0000

    Adding styles to contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,41 +2,47 @@ import "./Contact.css";
 
 export default function Contact() {
   return (
-    <div id="contact" className="flex-form page">
-      <form
-        action="https://getform.io/f/4276c753-0626-4eae-9a24-8fd9179f6ca4"
-        method="POST"
-      >
-        <div class="mb-3">
-          <input
-            type="text"
-            placeholder="Name"
-            className="form-control text"
-            name="name"
-          />
-          <br />
-          <input
-            type="email"
-            placeholder="Email Address"
-            className="form-control text"
-            name="email"
-          />
-          <br />
-          <div className="message-field">
-            <textarea
-              type="text"
-              rows="5"
-              placeholder="Your Message"
-              className="form-control text message"
-              name="message"
-            />
-          </div>
-          <br />
-          <button type="submit" class="btn">
-            Send
-          </button>
+    <>
+      <div id="contact" className="flex-form page">
+        <div className="heading-flex">
+        <h1 className="title-style">Contact</h1>
         </div>
-      </form>
-    </div>
+        <br />
+        <form
+          action="https://getform.io/f/4276c753-0626-4eae-9a24-8fd9179f6ca4"
+          method="POST"
+        >
+          <div class="mb-3">
+            <input
+              type="text"
+              placeholder="Name"
+              className="form-control text"
+              name="name"
+            />
+            <br />
+            <input
+              type="email"
+              placeholder="Email Address"
+              className="form-control text"
+              name="email"
+            />
+            <br />
+            <div className="message-field">
+              <textarea
+                type="text"
+                rows="5"
+                placeholder="Your Message"
+                className="form-control text message"
+                name="message"
+              />
+            </div>
+            <br />
+            <button type="submit" class="btn">
+              Send
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
   );
 }
