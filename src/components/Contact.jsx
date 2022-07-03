cca341bcc30d0ebb1e36725c36604123727cbca1
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-07-03 20:25:18 +0000

    Completed form creation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,34 +3,25 @@ import "./Contact.css"
 export default function Contact() {
   return (
     <div id="contact" className="flex-form page">
-    <form action="https://getform.io/f/4276c753-0626-4eae-9a24-8fd9179f6ca4" method="POST">
-    <label>Name</label>
-    <input type="text" name="name"/>
-    <label>Email</label>
-    <input type="email" name="email"/>
-    <label>Message</label>
-    <input type="text" name="message"/>
-    
+<form action="https://getform.io/f/4276c753-0626-4eae-9a24-8fd9179f6ca4" method="POST">
 
-    <button type="submit">Send Message</button>
+<div class="mb-3">
+    <input type="text" placeholder="Name" class="form-control" id="exampleInputName1"/>
+  </div>
+
+
+  <div class="mb-3">
+
+    <input type="email" placeholder="Email Address" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <div id="emailHelp" class="form-text"></div>
+  </div>
+
+  <div class="mb-3">
+    <input type="text" placeholder="Your Message" class="form-control" id="exampleInputMessage1"/>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 </div>
   )
 }
 
-<form action="https://getform.io/f/4276c753-0626-4eae-9a24-8fd9179f6ca4" method="POST">
-  <div class="mb-3">
-    <label class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1"/>
-  </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
