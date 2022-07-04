f5e18769a82bada2ab36a2e6897f4604909ea12a
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-07-04 19:03:46 +0000

    Fixing left nav links

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -1,16 +1,23 @@
-import Github from "./../assets/github-logo.png"
-import CV from "./../assets/cv.png"
-import LinkedIn from "./../assets/linkedin-logo.png"
+import Github from "./../assets/github-logo.png";
+import CV from "./../assets/cv.png";
+import LinkedIn from "./../assets/linkedin-logo.png";
 
-import './BottomNav.css'
+import "./BottomNav.css";
 
 export default function BottomNav() {
   return (
-    <>
-    <a href="https://github.com/abhiram-satha" target="_blank"><img className="bottom-nav" src={Github} alt="GitHub logo"/></a>
-    <a href="https://resume.creddle.io/resume/hvf63d7muel" target="_blank"><img className="bottom-nav2" src={CV} alt="Resume symbol"/></a>
-    <a href="https://linkedin.com/in/abhiramsatha" target="_blank"><img className="bottom-nav3" src={LinkedIn} alt="Linkedin logo"/></a>
-
-  </>
-  )
+    <div className="left-fixed-nav">
+      <div className="left-flex">
+      <a href="https://github.com/abhiram-satha" target="_blank">
+        <img className="bottom-nav" src={Github} alt="GitHub logo" />
+      </a>
+      <a href="https://resume.creddle.io/resume/hvf63d7muel" target="_blank">
+        <img className="bottom-nav2" src={CV} alt="Resume symbol" />
+      </a>
+      <a href="https://linkedin.com/in/abhiramsatha" target="_blank">
+        <img className="bottom-nav3" src={LinkedIn} alt="Linkedin logo" />
+      </a>
+      </div>
+    </div>
+  );
 }
