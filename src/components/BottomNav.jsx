3403492c62f689db79fdee1c2af67b7cd86fd91e
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-07-04 19:33:26 +0000

    Adding updates to left nav

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -11,9 +11,11 @@ export default function BottomNav() {
       <a href="https://github.com/abhiram-satha" target="_blank">
         <img className="bottom-nav" src={Github} alt="GitHub logo" />
       </a>
+      <br />
       <a href="https://resume.creddle.io/resume/hvf63d7muel" target="_blank">
         <img className="bottom-nav2" src={CV} alt="Resume symbol" />
       </a>
+      <br />
       <a href="https://linkedin.com/in/abhiramsatha" target="_blank">
         <img className="bottom-nav3" src={LinkedIn} alt="Linkedin logo" />
       </a>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,20 @@
-import "./Projects.css"
+import "./Projects.css";
+import FitWithUs from "./../assets/fitwithus-logo.png";
 
 export default function Projects() {
   return (
     <div id="projects" className="page">
       <div className="heading-flex flex-form">
         <h1 className="title-style">Projects</h1>
+      </div>
+
+      <div className="project-grid">
+        <div className="project-flex">
+        <a href="https://github.com/abhiram-satha/fit_with_us" target="_blank">
+          <img className="project-image" src={FitWithUs} />
+        </a>
+        <p>Fit With Us</p>
+        </div>
       </div>
     </div>
   );
