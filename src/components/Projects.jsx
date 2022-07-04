b48d80aea5763eb83575a46b1ef203cffc8b574b
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-07-04 20:24:22 +0000

    Fixed projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,8 @@
 import "./Projects.css";
+
 import FitWithUs from "./../assets/fitwithus-logo.png";
+import GrubGrab from "./../assets/grubgrab.png"
+import Tinyapp from "./../assets/tinyapp.png"
 
 export default function Projects() {
   return (
@@ -14,6 +17,20 @@ export default function Projects() {
           <img className="project-image" src={FitWithUs} />
         </a>
         <p>Fit With Us</p>
+        </div>
+
+        <div className="project-flex">
+        <a href="https://github.com/abhiram-satha/grubGrab" target="_blank">
+          <img className="project-image" src={GrubGrab} />
+        </a>
+        <p>Grub Grab</p>
+        </div>
+
+        <div className="project-flex">
+        <a href="https://github.com/abhiram-satha/tinyapp" target="_blank">
+          <img className="project-image" src={Tinyapp}/>
+        </a>
+        <p>TinyApp</p>
         </div>
       </div>
     </div>
