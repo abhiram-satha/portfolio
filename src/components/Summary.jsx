3eaa051e74059b88af81e21a9e73ddb518fe7e5c
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-07-05 21:43:51 +0000

    Changing summary

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -5,19 +5,19 @@ export default function Summary() {
       <h1>Abhiram Sathasivam.</h1>
       <p>
         After 7+ years of working professionally as an accountant, I realized I
-        was no longer fulfilled in the industry. That is when I decided to
-        rediscover my long lost passion for creating websites. To better enhance
-        and tone my skills, I enrolled into a web development bootcamp to learn
-        more about the back-end and database to become a truly rounded
-        Full-Stack Web Developer. My instructor had once mentioned as Web
-        Developers we are creating a better Internet for everyone and I truly
-        believe that to be true whether through accessibility or making apps
-        easier to use. And I continue to use this philosophy whenever I work on a
-        project. Currently seeking my first professional role in the industry.
+        was no longer fulfilled in the industry. I decided to uncover my long
+        lost passion for web development prior to my accounting days. After
+        enrolling in a web development bootcamp to enhance my skills and sharpen
+        my craft, where I learned more about the back-end and databases to
+        become a truly rounded Full Stack Web Developer.
+        <br />I truly believe Web Developers are leading the way to create a
+        better internet for everyone. As my mantra, I practice this everyday
+        whether that be through making websites I develop accessible or
+        improving the usability of my apps.
       </p>
       <p>
-        Feel free to take a look around and send me a message if you would like
-        to chat.
+        Checkout my portfolio below. If you're interested in my work, drop me a
+        <a className="para-text" href="#contact"> note</a>.
       </p>
     </div>
   );
