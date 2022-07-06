2e4ba820852db3b2545f4288f97fc5838c603fd4
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-07-06 22:22:51 +0000

    Removing portals

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,19 +4,9 @@ import FitWithUs from "./../assets/fitwithus-logo.png";
 import GrubGrab from "./../assets/grubgrab.png";
 import Tinyapp from "./../assets/tinyapp.png";
 
-import * as React from "react";
-import Box from "@mui/material/Box";
-import Portal from "@mui/material/Portal";
 
 export default function Projects() {
-  const [showfitforus, setshowfitforus] = React.useState(false);
-  const [showgrubgrab, setshowgrubgrab] = React.useState(false);
-  const container = React.useRef(null);
-
-  const handleClick = () => {
-    setshowfitforus(!showfitforus);
-    setshowgrubgrab(false)
-  };
+ 
 
   return (
     <div id="projects" className="page">
@@ -33,20 +23,7 @@ export default function Projects() {
             <img className="project-image" src={FitWithUs} />
           </a>
           <p>Fit With Us</p>
-          <div>
-      <button type="button" onClick={handleClick}>
-        {showfitforus ? 'Hide Case Study' : 'Show Case Study'}
-      </button>
-      <Box>
-
-        {showfitforus ? (
-          <Portal container={container.current}>
-            <span>But I actually render here!</span>
-          </Portal>
-        ) : null}
-      </Box>
-      <Box sx={{ p: 1, my: 1, border: '1px solid' }} ref={container} />
-    </div>
+          
         </div>
 
         <div className="project-flex">
@@ -54,20 +31,7 @@ export default function Projects() {
             <img className="project-image" src={GrubGrab} />
           </a>
           <p>Grub Grab</p>
-          <div>
-      <button type="button" onClick={handleClick}>
-        {showgrubgrab ? 'Hide Case Study' : 'Show Case Study'}
-      </button>
-      <Box>
-
-        {showgrubgrab ? (
-          <Portal container={container.current}>
-            <span>But I actually render here!</span>
-          </Portal>
-        ) : null}
-      </Box>
-      <Box  ref={container} />
-    </div>
+          
         </div>
 
         <div className="project-flex">
