3c82ba0c50e463b72b9f19585b9fd1c911bcdd2d
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-07-13 22:17:01 +0000

    Adding react popup modal

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,6 +4,8 @@ import FitWithUs from "./../assets/fitwithus-logo.png";
 import GrubGrab from "./../assets/grubgrab.png";
 import Tinyapp from "./../assets/tinyapp.png";
 import { useState } from "react";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
 
 const classNames = require("classnames");
 
@@ -49,9 +51,60 @@ export default function Projects() {
             <img className="project-image" src={FitWithUs} />
           </a>
           <p>Fit With Us</p>
+          <Popup
+            trigger={<button className="button"> Open Modal </button>}
+            modal
+            nested
+          >
+            {(close) => (
+              <div className="modal">
+                <button className="close" onClick={close}>
+                  &times;
+                </button>
+                <div className="header"> Modal Title </div>
+                <div className="content">
+                  {" "}
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Atque, a nostrum. Dolorem, repellat quidem ut, minima sint vel
+                  eveniet quibusdam voluptates delectus doloremque, explicabo
+                  tempore dicta adipisci fugit amet dignissimos?
+                  <br />
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Consequatur sit commodi beatae optio voluptatum sed eius
+                  cumque, delectus saepe repudiandae explicabo nemo nam libero
+                  ad, doloribus, voluptas rem alias. Vitae?
+                </div>
+                <div className="actions">
+                  <Popup
+                    trigger={<button className="button"> Trigger </button>}
+                    position="top center"
+                    nested
+                  >
+                    <span>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                      Beatae magni omnis delectus nemo, maxime molestiae dolorem
+                      numquam mollitia, voluptate ea, accusamus excepturi
+                      deleniti ratione sapiente! Laudantium, aperiam doloribus.
+                      Odit, aut.
+                    </span>
+                  </Popup>
+                  <button
+                    className="button"
+                    onClick={() => {
+                      console.log("modal closed ");
+                      close();
+                    }}
+                  >
+                    close modal
+                  </button>
+                </div>
+              </div>
+            )}
+          </Popup>
           <button className="btn" onClick={() => fitWithUsButton()}>
             Case Study
           </button>
+
           <div className={fitWithUsDisplay}>
             <p>
               A meal prep app that helps the user decide their meal for the week
