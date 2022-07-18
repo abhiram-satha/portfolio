5c10fd53e53569b69e090fc16383364754604133
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-07-18 18:17:39 +0000

    Changing button color on Contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,7 +37,7 @@ export default function Contact() {
               />
             </div>
             <br />
-            <button type="submit" className="btn">
+            <button type="submit" className="contact-button btn">
               Send
             </button>
           </div>
