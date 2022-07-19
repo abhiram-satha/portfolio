fc989e9f59ccd10d78dbe0e17491469d4108b36d
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-07-19 22:39:44 +0000

    final changes to portfolio

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,7 +16,7 @@ export default function Projects() {
   return (
     <div id="projects" className="">
       <div className="heading-flex flex-form">
-        <h1 className="title-style">Projects</h1>
+        <h1 className="project title-style">Projects</h1>
       </div>
 
       <div className="project-grid">
